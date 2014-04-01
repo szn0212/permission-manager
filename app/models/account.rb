88bc15aab7d796ac80ea8bf847e217cf686f23cb
huaoguo <huaoguo@gmail.com>
2014-04-01 10:31:34 +0800

    缩进统一为2个空格

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,8 +1,8 @@
 class Account < ActiveRecord::Base
-	has_one :user
-    has_many :account_roles
-	has_many :roles, :through => :account_roles
-    has_many :account_permissions
-	has_many :permissions, :through => :account_permissions
-	has_many :tasks
+  has_one :user
+  has_many :account_roles
+  has_many :roles, :through => :account_roles
+  has_many :account_permissions
+  has_many :permissions, :through => :account_permissions
+  has_many :tasks
 end
