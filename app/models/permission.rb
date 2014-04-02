3f55c64e32d4799cfd6a1fbf2ba01618b02b3331
huaoguo <huaoguo@gmail.com>
2014-04-02 16:43:53 +0800

    修改了缩进

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -1,6 +1,6 @@
 class Permission < ActiveRecord::Base
   has_many :account_permissions
   has_many :accounts, :through => :account_permissions
-	has_many :role_permissions
-	has_many :roles, :through => :role_permissions
+  has_many :role_permissions
+  has_many :roles, :through => :role_permissions
 end
